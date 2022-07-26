5dad66ec2a1683ab0767ec095f6318900aac006d
Nurbek Abdirasulov <>
2022-07-26 18:16:02 +0600

    first commit

---
+++ src/main/java/com/company/webSocket/model/ChatMessage.java
@@ -0,0 +1,42 @@
+package com.company.webSocket.model;
+
+/**
+ * @Author: Nurbek Abdirasulov
+ * DATE: 26.07.2022
+ */
+
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
